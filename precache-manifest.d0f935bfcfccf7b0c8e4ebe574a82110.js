7bad5d1648dd5a27c5019aac70e115d9466be9fd
Devagnos <yosra.m@devagnos.com>
2020-11-08 15:44:55 +0100

    Updates

---
+++ precache-manifest.d0f935bfcfccf7b0c8e4ebe574a82110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3adc15c4d97dd6cd4281c984cdcfc9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b00f9c1fd0583205d5e",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "2b00f9c1fd0583205d5e",
+    "url": "/static/js/2.af9c6c85.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.af9c6c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b35006d6459b2cbe3e1e",
+    "url": "/static/js/main.a00b6c30.chunk.js"
+  },
+  {
+    "revision": "d06c4795e995394af7fa",
+    "url": "/static/js/runtime-main.d6958a7e.js"
+  }
+]);
